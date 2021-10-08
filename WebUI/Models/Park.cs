2cd0242908f75f252c35c32bc93a1894b612e43b
MariyaBukh <85184644+MariyaBukh@users.noreply.github.com>
2021-10-08 10:40:42 -0400

    created models

---
+++ WebUI/Models/Park.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+    {
+    public class Park
+        {
+        public string adjective { get; set; }
+        public string nounPl { get; set; }
+        public string noun { get; set; }
+        public string adverb { get; set; }
+        public string number { get; set; }
+        public string verbPt { get; set; }
+        public string adjective2 { get; set; }
+        public string verbPt2 { get; set; }
+        public string adverb2 { get; set; }
+        public string adjective4 { get; set; }
+        }
+    }
