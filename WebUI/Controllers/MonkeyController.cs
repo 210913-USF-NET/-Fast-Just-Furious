ad9e11c4abc957600074ee13f3dd1da42f639d86
MariyaBukh <85184644+MariyaBukh@users.noreply.github.com>
2021-10-08 13:40:52 -0400

    created all controllers

---
+++ WebUI/Controllers/MonkeyController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+    {
+    public class MonkeyController : Controller
+        {
+        // GET: MonkeyController
+        public ActionResult Index()
+            {
+            return View();
+            }
+
+        // GET: MonkeyController/Details/5
+        public ActionResult Details(int id)
+            {
+            return View();
+            }
+
+        // GET: MonkeyController/Create
+        public ActionResult Create()
+            {
+            return View();
+            }
+
+        // POST: MonkeyController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+            {
+            try
+                {
+                return RedirectToAction(nameof(Index));
+                }
+            catch
+                {
+                return View();
+                }
+            }
+
+        // GET: MonkeyController/Edit/5
+        public ActionResult Edit(int id)
+            {
+            return View();
+            }
+
+        // POST: MonkeyController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+            {
+            try
+                {
+                return RedirectToAction(nameof(Index));
+                }
+            catch
+                {
+                return View();
+                }
+            }
+
+        // GET: MonkeyController/Delete/5
+        public ActionResult Delete(int id)
+            {
+            return View();
+            }
+
+        // POST: MonkeyController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+            {
+            try
+                {
+                return RedirectToAction(nameof(Index));
+                }
+            catch
+                {
+                return View();
+                }
+            }
+        }
+    }
---
+++ WebUI/Controllers/ParkController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+    {
+    public class ParkController : Controller
+        {
+        // GET: ParkController
+        public ActionResult Index()
+            {
+            return View();
+            }
+
+        // GET: ParkController/Details/5
+        public ActionResult Details(int id)
+            {
+            return View();
+            }
+
+        // GET: ParkController/Create
+        public ActionResult Create()
+            {
+            return View();
+            }
+
+        // POST: ParkController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+            {
+            try
+                {
+                return RedirectToAction(nameof(Index));
+                }
+            catch
+                {
+                return View();
+                }
+            }
+
+        // GET: ParkController/Edit/5
+        public ActionResult Edit(int id)
+            {
+            return View();
+            }
+
+        // POST: ParkController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+            {
+            try
+                {
+                return RedirectToAction(nameof(Index));
+                }
+            catch
+                {
+                return View();
+                }
+            }
+
+        // GET: ParkController/Delete/5
+        public ActionResult Delete(int id)
+            {
+            return View();
+            }
+
+        // POST: ParkController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+            {
+            try
+                {
+                return RedirectToAction(nameof(Index));
+                }
+            catch
+                {
+                return View();
+                }
+            }
+        }
+    }
