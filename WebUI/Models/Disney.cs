fbe74dc830784ae2393818e1ef09a0dc5d8f5d5c
MariyaBukh <85184644+MariyaBukh@users.noreply.github.com>
2021-10-08 13:02:52 -0400

    added all models

---
+++ WebUI/Models/Disney.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+    {
+    public class Disney
+        {
+        public string name { get; set; }
+        public string number { get; set; }
+        public string vehicle { get; set; }
+        public string adjective { get; set; }
+        public string adjective2 { get; set; }
+        public string verb { get; set; }
+        public string animal { get; set; }
+        public string adjective3 { get; set; }
+        public string verbPt { get; set; }
+        public string adjective4 { get; set; }
+        public string name2 { get; set; }
+        public string verbPt2 { get; set; }
+        public string verbPt3 { get; set; }
+        public string place { get; set; }
+        public string verb2 { get; set; }
+
+        }
+    }
---
+++ WebUI/Models/Monkey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+    {
+    public class Monkey
+        {
+        public string verb { get; set; }
+        public string noun { get; set; }
+        public string adjective { get; set; }
+        public string adverb { get; set; }
+       
+        }
+    }
