2d50d4627e179e42738632b6efdbd9fc797b4f80
MariyaBukh <85184644+MariyaBukh@users.noreply.github.com>
2021-10-08 11:59:16 -0400

    fixed

---
+++ WebUI/Models/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+    {
+    public class Game
+        {
+        public string adjective { get; set; }
+        public string adjective2 { get; set; }
+        public string firstName { get; set; }
+        public string verbPt { get; set; }
+        public string firstName2 { get; set; }
+        public string adjective3 { get; set; }
+        public string adjective4 { get; set; }
+        public string nounPl { get; set; }
+        public string largeAnimal { get; set; }
+        public string smallAnimal { get; set; }
+        public string girlName { get; set; }
+        public string adjective5 { get; set; }
+        public string nounPl2 { get; set; }
+        public string adjective6 { get; set; }
+        public string nounPl3 { get; set; }
+        public string number { get; set; }
+        public string firstName3 { get; set; }
+        public string number2 { get; set; }
+        public string nounPl4 { get; set; }
+        public string number3 { get; set; }
+        public string nounPl5 { get; set; }
+        }
+    }
---
+++ WebUI/Models/Jungle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+    {
+    public class Jungle
+        {
+        public string adjective { get; set; }
+        public string adjective2 { get; set; }
+        public string adjective3 { get; set; }
+        public string noun { get; set; }
+        public string adjective4 { get; set; }
+        public string adjective5 { get; set; }
+        public string noun2 { get; set; }
+        public string verb { get; set; }
+        public string verb2 { get; set; }
+        public string adjective6 { get; set; }
+        public string noun3 { get; set; }
+        public string verb3 { get; set; }
+        public string noun4 { get; set; }
+        public string verb4 { get; set; }
+        public string adjective7 { get; set; }
+        }
+    }
